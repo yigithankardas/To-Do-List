fc7d8e2173a07258d73827c402876a6942b4445d
yigithankardas <y.kardas@etu.edu.tr>
2022-02-28 12:04:56 +0300

    gui: create prototype

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -2,25 +2,77 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class GUI {
+
+    private ArrayList<String> list;
+
+    private JFrame frame;
+
+    private JPanel mainPanel;
+    private JPanel descriptionPanel;
+    private JPanel deadlinePanel;
+    private JPanel clockPanel;
+
+    private JLabel deadlineLabel;
+    private JLabel clockLabel;
+
+    private JTextField descriptionField;
+
+    private Color gray;
+
     public GUI() {
-        JFrame frame = new JFrame();
+        list = new ArrayList<>();
+        frame = new JFrame();
+
+        mainPanel = new JPanel();
+        descriptionPanel = new JPanel();
+        deadlinePanel = new JPanel();
+        clockPanel = new JPanel();
+
+        deadlineLabel = new JLabel();
+        clockLabel = new JLabel();
+
+        descriptionField = new JTextField();
+
+        gray = new Color(40, 40, 40);
+        frame.setResizable(false);
+        arrange();
+        frame.setSize(500, 390);
+        frame.setSize(500, 400);
+    }
+
+    private void arrange() {
         frame.setDefaultCloseOperation(3);
-        frame.setBounds(500, 25, 500, 400);
+        frame.setBounds(500, 100, 500, 400);
 
-        JPanel panel = new JPanel();
-        panel.setBackground(new Color(40, 40, 40));
-        frame.add(panel);
+        mainPanel.setBackground(gray);
+        mainPanel.setLayout(null);
+        descriptionPanel.setBackground(gray);
+        descriptionPanel.setLayout(null);
+        descriptionPanel.setBorder(BorderFactory.createLineBorder(Color.red));
+        descriptionPanel.setBounds(1, 80, 485, 282);
+        deadlinePanel.setBackground(gray);
+        deadlinePanel.setLayout(null);
+        deadlinePanel.setBorder(BorderFactory.createLineBorder(Color.blue));
+        deadlinePanel.setBounds(1, 1, 378, 80);
+        clockPanel.setBackground(gray);
+        clockPanel.setLayout(null);
+        clockPanel.setBorder(BorderFactory.createLineBorder(Color.yellow));
+        clockPanel.setBounds(380, 0, 105, 80);
 
-        JLabel label = new JLabel();
-        label.setText("Hello new project!");
-        label.setFont(new Font("Seriff", Font.ITALIC, 30));
-        label.setForeground(Color.white);
-        panel.add(label);
-
+        descriptionField.setBackground(Color.black);
+        descriptionField.setForeground(Color.white);
+        descriptionField.setCaretColor(Color.gray);
+        descriptionField.setFont(new Font("Seriff", Font.PLAIN, 25));
+        descriptionField.setBounds(1, 1, 482, 280);
+        descriptionField.setBorder(BorderFactory.createEmptyBorder());
+        descriptionPanel.add(descriptionField);
+        mainPanel.add(descriptionPanel);
+        mainPanel.add(clockPanel);
+        mainPanel.add(deadlinePanel);
+        frame.add(mainPanel);
         frame.setVisible(true);
-        frame.setSize(500, 398);
-        frame.setSize(500, 440);
     }
 }
